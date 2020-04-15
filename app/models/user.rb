d5ceb3064d47cec6d1364ee617426a06647b1ce5
fukuba0319 <mizubuka0319@gmail.com>
2020-04-15 09:59:55 +0000

    登録フォーム

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 20}
   validates :email, presence: true, format:{with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  
+  has_secure_password
+  validates :password, presence: true
+  validates :password, length: {in: 6..32}
+  validates :password, format:{with: /\A[a-zA-Z0-9]+\z/}  
 end
